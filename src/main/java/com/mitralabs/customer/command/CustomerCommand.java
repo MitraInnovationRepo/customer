6cb3eeee5dcea9f2c18a94adecff5f673d09d13b
lherath@mitrai.com <lherath@mitrai.com>
2021-03-10 18:37:53 +0530

    new changes

--- src/main/java/com/mitralabs/customer/command/CustomerCommand.java
+++
@@ -1,8 +0,0 @@
-package com.mitralabs.customer.command;
-
-import io.eventuate.Command;
-
-public interface CustomerCommand extends Command {
-
-
-}
--- src/main/java/com/mitralabs/customer/event/CustomerEvent.java
+++
@@ -1,8 +0,0 @@
-package com.mitralabs.customer.event;
-
-import io.eventuate.Event;
-import io.eventuate.EventEntity;
-
-@EventEntity(entity = "com.mitralabs.customer.CustomerAggregate")
-public interface CustomerEvent extends Event {
-}
